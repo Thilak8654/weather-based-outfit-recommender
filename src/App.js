d6ee93cafaa167ffaddbb6bb68b4d425c2610e80
Thilak Raj <admin@admins-MacBook-Pro.local>
2025-08-02 13:22:10 +0530

    First skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="mx-auto my-5 p-4 bg-white">
+       <h1 className="text-2xl font-bold text-center mb-6">
+        🌤️ Weather-Based Outfit Recommender
+      </h1>
+    </div>
+  );
+}
+
+export default App;
